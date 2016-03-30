d0e885bd77d56455a59455a252de8510159ad867
Clemens Hemmerling <clemens@galileo.edu>
2016-03-29 22:54:19 -0600

    Agregamos la descripcion para movil en la seccion 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,8 +11,11 @@
   function onDOMLoad() {
     var btnMenu = document.getElementById('btnMenu');
     var navbarMenu = document.getElementById('navbarMenu');
+    var divtext = document.getElementById('divtext');
+    var hide = document.getElementById('hide');
 
     btnMenu.addEventListener('click', onClickMenu);
+    divtext.addEventListener('click', onClickSect);
 
     loadCSS('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css');
     loadCSS('https://fonts.googleapis.com/css?family=Muli|Roboto');
@@ -21,7 +24,10 @@
       navbarMenu.classList.toggle('header-menu-list--show');
     }
 
-  }
+    function onClickSect(){
+      hide.classList.toggle('sectTwo-hide--show')
+    }
 
+  }
 
 }());
