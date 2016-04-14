3db20433377e1c390bae2eb1a42946b023c1b6fa
Clemens Hemmerling <clemens@galileo.edu>
2016-04-14 13:14:55 -0600

    Agregamos nuevas optimizaciones en el package.json y gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -7,43 +7,49 @@ var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var uglify = require('gulp-uglify');
+var smoosher = require('gulp-smoosher');
+var imageop = require('gulp-image-optimization');
 
 var config = {
-  styles:{
+  styles: {
     main: './src/styles/main.styl',
     watch: './src/styles/**/*.styl',
     output: './build/css'
   },
-  html:{
+  html: {
     watch: './build/*.html'
   },
-  scripts:{
+  scripts: {
     main: './src/scripts/main.js',
     watch: './src/script/**/*.js',
     output: './build/js'
+  },
+  images: {
+    watch: ['./build/img/*.png', './build/img/*.jpg'],
+    output: './dist/img'
   }
 }
 
-gulp.task('server', function(){
+gulp.task('server', function() {
   gulp.src('./build')
     .pipe(webserver({
       host: '0.0.0.0',
-      port:8080,
+      port: 8080,
       livereload: true
     }));
 });
 
-gulp.task('build:css', function(){
+gulp.task('build:css', function() {
   gulp.src(config.styles.main)
     .pipe(stylus({
       use: nib(),
-        'include css': true
+      'include css': true
     }))
-      .pipe(minifyCSS())
-      .pipe(gulp.dest(config.styles.output));
+    .pipe(minifyCSS())
+    .pipe(gulp.dest(config.styles.output));
 });
 
-gulp.task('build:js', function(){
+gulp.task('build:js', function() {
   return browserify(config.scripts.main)
     .bundle()
     .pipe(source('bundle.js'))
@@ -52,12 +58,29 @@ gulp.task('build:js', function(){
     .pipe(gulp.dest(config.scripts.output));
 });
 
-gulp.task('watch', function(){
-  gulp.watch(config.scripts.watch, ['build:js'])
+gulp.task('watch', function() {
+  gulp.watch(config.images.watch, ['images']);
+  gulp.watch(config.scripts.watch, ['build:js']);
   gulp.watch(config.styles.watch, ['build:css']);
   gulp.watch(config.html.watch, ['build']);
-})
+});
 
-gulp.task('build', ['build:css', 'build:js'])
+gulp.task('images', function() {
+  gulp.src(config.images.watch)
+    .pipe(imageop({
+      optimizationLevel: 5,
+      progressive: true,
+      interlaced: true
+    }))
+    .pipe(gulp.dest(config.images.output));
+});
+
+gulp.task('inline', function() {
+  gulp.src('./build/index.html')
+    .pipe(smoosher())
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('build', ['build:css', 'build:js', 'images', 'inline']);
 
 gulp.task('default', ['server', 'watch', 'build']);
